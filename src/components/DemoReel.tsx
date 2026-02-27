73888afbf25fbf68616dbbfec194546055194da9
skngh <sknight3637@gmail.com>
2026-02-27 17:27:55 -0500

    added demo reel

--- src/components/DemoReel.tsx
+++ src/components/DemoReel.tsx
@@ -4,7 +4,13 @@ export default function DemoReel() {
   return (
     <div className="demo-reel">
       <div className="video-placeholder">
-        <p>Placeholder</p>
+        <iframe
+          src="https://www.youtube.com/embed/Fo-s3UjHY-Q?si=5Y6gMl46pHSVLA0A"
+          title="Sound Design Demo Reel"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+        />
       </div>
     </div>
   );
